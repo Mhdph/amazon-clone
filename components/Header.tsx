40553c8ce41f7adb2898125314351cb6597d9c5b
mahdi parivash <mahdiparivash@protonmail.com>
2022-07-05 15:42:38 +0430

    complate google auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,17 @@ import {
 } from "@heroicons/react/outline";
 import { useSession, signIn, signOut } from "next-auth/react";
 type HeaderProps = {};
+import { useRouter } from "next/router";
 
 const Header: React.FC<HeaderProps> = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0 ">
           <Image
+            onClick={() => router.push("/")}
             className="cursor-pointer"
             width={150}
             height={40}
@@ -29,19 +33,22 @@ const Header: React.FC<HeaderProps> = () => {
           <SearchIcon className="h-12 p-4" />
         </div>
 
-        <div
-          onClick={() => signIn()}
-          className="flex text-white items-center text-xs space-x-6 mx-6 whitespace-nowrap"
-        >
-          <div className="link">
-            <p>Hello Mahdi Parivsh</p>
+        <div className="flex text-white items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div
+            onClick={!session ? () => signIn() : () => signOut()}
+            className="link"
+          >
+            <p>{session ? `Hello,${session.user?.name}` : "Sign In"}</p>
             <p className="font-extrabold md:text-sm">Account & List</p>
           </div>
           <div className="link">
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="flex relative link items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="flex relative link items-center"
+          >
             <ShoppingCartIcon className="h-10" />
             <span className="absolute bg-yellow-400 top-0 right-0 md:right-10 h-4 w-4 text-center rounded-full text-black font-bold">
               4
